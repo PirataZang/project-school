b58c982739108247fa42efe6a2932fbba20136cd
PirataZang <igorfronza20@gmail.com>
2024-12-20 16:21:02 -0300

    Adicionado Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,62 @@
 <template>
     <div class="footer">
-        <h1>Footer</h1>
-
-
-         <div class="socialMedias">
-            <ul>
-                <li>SocialMedia</li>
-                <li>SocialMedia</li>
-                <li>SocialMedia</li>
-                <li>SocialMedia</li>
-                <li>SocialMedia</li>
+        <h1>Logo</h1>
+        <div class="socialMedias column">
+            <h1>Atendimento</h1>
+            <ul class="socialMediasOptions">
+                <li class="socialMedia">
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-instagram"></i> Instagram
+                    </a>
+                </li>
+                <li class="socialMedia">
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-square-facebook"></i> Facebook
+                    </a>
+                </li>
+                <li class="socialMedia">
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-square-facebook"></i> Facebook
+                    </a>
+                </li>
+                <li class="socialMedia column">
+                    <textarea class="textarea" v-model="userMessage" placeholder="Escreva aqui sua mensagem"></textarea>
+                    <button class="buttonWhatsapp">
+                        <a class="send" :href="whatsAppLink" target="_blank"><i class="fa-brands fa-whatsapp fa-xl"></i>
+                            Enviar mensagem pelo WhatsApp</a>
+                    </button>
+                </li>
             </ul>
-         </div>
-         <div class="contacts">
-            <ul>
-                <li>Contact</li>
-                <li>Contact</li>
-                <li>Contact</li>
-            </ul>
-         </div>
+        </div>
+        <div class="contacts column">
+            <h1>Contatos</h1>
+            <a class="contact" href="tel:+5547989022030"> <i class="fa-solid fa-phone" style="color: #ffffff;"></i> (47) 989022030 </a>
+            <a class="contact" href="https://wa.me/47989022030"> <i class="fa-brands fa-whatsapp"></i> (47) 989022030 </a>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Footer',
+
+    data() {
+        return {
+            phoneNumber: 47989022030,
+            userMessage: 'Olá Vim pelo Site'
+        }
+    },
+
+    methods: {
+        encodeMessage(message) {
+            return encodeURIComponent(message); // Converte os caracteres especiais para a URL
+        },
+    },
+
+    computed: {
+        whatsAppLink() {
+            return `https://wa.me/${this.phoneNumber}?text=${this.encodeMessage(this.userMessage)}`;
+        },
+    },
 }
 </script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="menu">
         <div class="contacts step row f-18 fWhite">
-            <router-link to="/" class="router-link">Fale Conosco</router-link>
             <router-link to="/KARAIO" class="router-link">Portal</router-link>
-            <router-link to="/" class="router-link">Trabalhe com a gente</router-link>
+            <router-link to="/" class="router-link">Trabalhe Conosco</router-link>
         </div>
         <div class="actions step row fWhite ">
             <div class="logo">
@@ -11,7 +10,7 @@
             </div>
             <ul class="actionsButtons row f-18">
                 <li @click="showOptionsSchool = !showOptionsSchool" class="school">
-                    Escola   <i id="arrowSchool" class="fa-solid fa-caret-up arrow down"></i>
+                    Escola <i id="arrowSchool" class="fa-solid fa-caret-up arrow down"></i>
                     <div v-if="showOptionsSchool" class="schoolOptions column">
                         <p>História</p>
                         <p>Colonia de férias</p>
@@ -39,8 +38,9 @@ export default {
 
     watch: {
         'showOptionsSchool': function () {
-            document.getElementById('arrowSchool').classList.toggle('down');
+            document.getElementById('arrowSchool').classList.toggle('down')
         }
+
     }
 }
 </script>
